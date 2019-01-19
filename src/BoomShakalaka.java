8470deebf64aa3c17a00c3908091702e8d0f29e5
Epic_fail^ <kas.eca-electra@inbox.ru>
2019-01-19 23:48:13 +0300

    Stream safety?

---
+++ src/BoomShakalaka.java
@@ -0,0 +1,6 @@
+public class BoomShakalaka {
+    public static void main(String[] args) {
+        PlzGodsNo rlyPlzGodsNo = new PlzGodsNo();
+        System.out.println(rlyPlzGodsNo.powWow());
+    }
+}
---
+++ src/PlzGodsNo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlzGodsNo {
+
+    private List pow = new ArrayList<String>();
+
+    public List getPow() {
+        return pow;
+    }
+
+    public List powWow() {
+        pow.add ("Wow!");
+        pow.add("Fuf..");
+        pow.add("Holly trolly!!");
+        return pow;
+    }
+}
