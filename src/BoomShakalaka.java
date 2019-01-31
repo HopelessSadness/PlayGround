56fabbcc1bc73c808663e9b1ce548cad1b62db4e
Epic_fail^ <kas.eca-electra@inbox.ru>
2019-01-31 15:13:15 +0300

    Some fn wth equals&&hashCode?

--- src/BoomShakalaka.java
+++ src/BoomShakalaka.java
@@ -1,6 +1,17 @@
 public class BoomShakalaka {
     public static void main(String[] args) {
-        PlzGodsNo rlyPlzGodsNo = new PlzGodsNo();
-        System.out.println(rlyPlzGodsNo.powWow());
+        PlzGodsNo rlyPlzGodsNo0 = new PlzGodsNo("Meow",1488);
+        PlzGodsNo rlyPlzGodsNo1 = new PlzGodsNo("Lol",1488);
+        PlzGodsNo rlyPlzGodsNo2 = new PlzGodsNo("Hell yeah", 666);
+        PlzGodsNo rlyPlzGodsNo3 = new PlzGodsNo("Lol",1488);
+        PlzGodsNo rlyPlzGodsNo4 = rlyPlzGodsNo3;
+
+        System.out.println(rlyPlzGodsNo0.hashCode());
+        System.out.println(rlyPlzGodsNo1.hashCode());
+        System.out.println(rlyPlzGodsNo2.hashCode());
+        System.out.println(rlyPlzGodsNo3.hashCode());
+        System.out.println(rlyPlzGodsNo4.hashCode());
+        System.out.println(rlyPlzGodsNo4.equals(rlyPlzGodsNo1));
+        System.out.println(rlyPlzGodsNo4.equals(rlyPlzGodsNo0));
     }
 }
--- src/PlzGodsNo.java
+++ src/PlzGodsNo.java
@@ -4,15 +4,57 @@ import java.util.List;
 public class PlzGodsNo {
 
     private List pow = new ArrayList<String>();
+    private String nameOfVariable;
+    private int valueOfVariable;
+
+    public String getNameOfVariable() {
+        return nameOfVariable;
+    }
+
+    public int getValueOfVariable() {
+        return valueOfVariable;
+    }
+
+    public void setNameOfVariable(String nameOfVariable) {
+        this.nameOfVariable = nameOfVariable;
+    }
+
+    public void setValueOfVariable(int valueOfVariable) {
+        this.valueOfVariable = valueOfVariable;
+    }
+
+    public PlzGodsNo(String nameOfVariable, int valueOfVariable) {
+        setNameOfVariable(nameOfVariable);
+        setValueOfVariable(valueOfVariable);
+    }
 
     public List getPow() {
         return pow;
     }
 
     public List powWow() {
-        pow.add ("Wow!");
+        pow.add("Wow!");
         pow.add("Fuf..");
         pow.add("Holly trolly!!");
         return pow;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlzGodsNo plzEql = (PlzGodsNo) o;
+        return this.getValueOfVariable() == plzEql.getValueOfVariable() && this.getNameOfVariable().equals(plzEql.getNameOfVariable());
+    }
+
+    @Override
+    public int hashCode() {
+        final int hshConst = 31;
+        int result = 1;
+
+        result = hshConst * result + this.getValueOfVariable();
+        result = hshConst * result + this.getNameOfVariable().hashCode();
+
+        return result;
     }
 }
