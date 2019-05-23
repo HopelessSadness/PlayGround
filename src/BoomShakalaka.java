3177f2ea0444dc849405928d6d1739377759563c
Epic_fail^ <kas.eca-electra@inbox.ru>
2019-05-23 16:36:03 +0300

    +some jdbc (to be cont..)?

--- src/BoomShakalaka.java
+++ src/BoomShakalaka.java
@@ -1,9 +1,30 @@
+import java.nio.channels.Selector;
+import java.sql.*;
+
 public class BoomShakalaka {
-    public static void main(String[] args) {
-        PlzGodsNo rlyPlzGodsNo0 = new PlzGodsNo("Meow",1488);
-        PlzGodsNo rlyPlzGodsNo1 = new PlzGodsNo("Lol",1488);
+    public static void main(String[] args) throws SQLException {
+        String userName = "root";
+        String password = "...."; // << best pswrd ever was here (btw bd idea)
+        String connectionUrl = "jdbc:mysql://localhost:3306/mySql?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true"; //ts danger xD
+        //Class.forName("com.mysql.jdbc.Driver"); //ts works bt wtf "This is deprecated"? xD
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password)) {
+            System.out.println("Holly trolly!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM boom.boom");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String value = resultSet.getString("value");
+                System.out.println(id + " " + value);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        PlzGodsNo rlyPlzGodsNo0 = new PlzGodsNo("Meow", 1488);
+        PlzGodsNo rlyPlzGodsNo1 = new PlzGodsNo("Lol", 1488);
         PlzGodsNo rlyPlzGodsNo2 = new PlzGodsNo("Hell yeah", 666);
-        PlzGodsNo rlyPlzGodsNo3 = new PlzGodsNo("Lol",1488);
+        PlzGodsNo rlyPlzGodsNo3 = new PlzGodsNo("Lol", 1488);
         PlzGodsNo rlyPlzGodsNo4 = rlyPlzGodsNo3;
 
         System.out.println(rlyPlzGodsNo0.hashCode());
